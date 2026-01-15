df6a8c7197103ec2cf70e5846e02a1d0dcb5ff93
s1lentq <s1lentsk@yandex.ru>
2026-01-15 16:18:05 +0700

    refactor: clean up UserMsg for better readability

--- src/engine/host_cmd.cpp
+++ src/engine/host_cmd.cpp
@@ -2697,7 +2697,7 @@ void Host_Say(qboolean teamonly)
 
 		host_client = client;
 
-		PF_MessageBegin_I(MSG_ONE, RegUserMsg("SayText", -1), NULL, &g_psv.edicts[j + 1]);
+		PF_MessageBegin_I(MSG_ONE, SV_RegUserMsg("SayText", -1), NULL, &g_psv.edicts[j + 1]);
 		PF_WriteByte_I(0);
 #ifdef REHLDS_FIXES
 		PF_WriteString_I(preparedText);
@@ -2778,7 +2778,7 @@ void Host_Tell_f(void)
 
 		host_client = client;
 
-		PF_MessageBegin_I(MSG_ONE, RegUserMsg("SayText", -1), NULL, &g_psv.edicts[j + 1]);
+		PF_MessageBegin_I(MSG_ONE, SV_RegUserMsg("SayText", -1), NULL, &g_psv.edicts[j + 1]);
 		PF_WriteByte_I(0);
 		PF_WriteString_I(text);
 		PF_MessageEnd_I();
