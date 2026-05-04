00a6d182b3a322056d495d4bcf469534d0400781
the_hunter <43341889+hun1er@users.noreply.github.com>
2026-05-04 20:30:44 +0300

    fix: align DT_TIMEWINDOW with sv_rehlds_local_gametime (#1148)

--- src/engine/sv_main.cpp
+++ src/engine/sv_main.cpp
@@ -5008,7 +5008,13 @@ qboolean SV_SendClientDatagram(client_t *client)
 #ifdef REHLDS_FIXES
 	if (sv_rehlds_local_gametime.value != 0.0f)
 	{
-		MSG_WriteFloat(&msg, (float)g_GameClients[client - g_psvs.clients]->GetLocalGameTime());
+		CGameClient* gameClient = g_GameClients[client - g_psvs.clients];
+
+		double localGameTime = gameClient->GetLocalGameTime();
+		double localGameTimeBase = gameClient->GetLocalGameTimeBase();
+		DELTA_SetTimeBaseOverride(localGameTime, localGameTimeBase);
+
+		MSG_WriteFloat(&msg, (float)localGameTime);
 	}
 	else
 #endif
@@ -5044,6 +5050,10 @@ qboolean SV_SendClientDatagram(client_t *client)
 	}
 
 	Netchan_Transmit(&client->netchan, msg.cursize, buf);
+
+#ifdef REHLDS_FIXES
+	DELTA_ClearTimeBaseOverride();
+#endif
 
 	return TRUE;
 }
